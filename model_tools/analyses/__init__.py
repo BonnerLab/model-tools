3695d286206c682bfab24f918d64820fbef35e92
EricElmoznino <eric.elmoznino@gmail.com>
2022-06-08 14:09:01 -0400

    - Removed multithreading support from analyses (since this would have caused memory issues with many layers simultaneously being loaded into RAM). - Fixed a potential bug where successive layers were all being loaded into memory. Unsure if this is definitely fixed, though.

--- model_tools/analyses/__init__.py
+++ model_tools/analyses/__init__.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import logging
+from collections import OrderedDict
 from typing import Any, Union, Optional, List, Dict
 from pathlib import Path
 
@@ -9,7 +10,6 @@ from tqdm import tqdm
 from brainio.stimuli import StimulusSet
 from brainio.assemblies import NeuroidAssembly
 from model_tools.activations import PytorchWrapper, TensorflowWrapper, TensorflowSlimWrapper, KerasWrapper
-from model_tools.activations.core import change_dict
 from model_tools.utils import fullname
 from result_caching import store_dict
 
@@ -37,7 +37,7 @@ class ModelActivationsAnalysis(ABC):
         pass
 
     @property
-    def results(self) -> Dict[str, Any]:
+    def results(self) -> Any:
         """
         This property can be overidden in subclasses if you wish to transform the format of the results
         (e.g. merge the dictionary into an xarray DataArray).
@@ -56,8 +56,7 @@ class ModelActivationsAnalysis(ABC):
                  stimuli: Union[StimulusSet, List[str], List[Path]],
                  layers: List[str],
                  stimuli_identifier: Optional[Union[bool, str]] = None,
-                 multithread: bool = False,
-                 **kwargs) -> Dict[str, Any]:
+                 **kwargs) -> Any:
         """
         Perform an analysis on a model given a set of stimuli.
         :param stimuli: Stimuli to obtain activations from.
@@ -65,9 +64,8 @@ class ModelActivationsAnalysis(ABC):
         :param stimuli_identifier: Identifier used for stimulus set (used for caching filename).
             Setting to False will prevent activations and analysis results from being cached.
             If caching is desired, either 'stimuli' must be a StimulusSet or 'stimuli_identifier' must be a string.
-        :param multithread: Whether or not to multithread analysis across layers.
         :param kwargs: Additional arguments that are passed down to 'self.analysis_func()'.
-        :return: A dictionary mapping layer names to arbitrarily-structured analysis results.
+        :return: Arbitrary analysis data structure.
         """
         if isinstance(stimuli, StimulusSet) and stimuli_identifier is None:
             stimuli_identifier = stimuli.identifier
@@ -77,30 +75,28 @@ class ModelActivationsAnalysis(ABC):
                                                             stimuli=stimuli,
                                                             layers=layers,
                                                             stimuli_identifier=stimuli_identifier,
-                                                            multithread=multithread,
                                                             analysis_kwargs=kwargs)
             self._metadata['stimuli_identifier'] = stimuli_identifier
         else:
             self._layer_results = self._run_analysis(stimuli=stimuli,
                                                      layers=layers,
                                                      stimuli_identifier=stimuli_identifier,
-                                                     multithread=multithread,
                                                      **kwargs)
 
         return self.results
 
-    @store_dict(dict_key='layers', identifier_ignore=['stimuli', 'layers', 'multithread'])
+    @store_dict(dict_key='layers', identifier_ignore=['stimuli', 'layers'])
     def _run_analysis_stored(self,
                              identifier: str,
                              stimuli: Union[StimulusSet, List[str], List[Path]],
                              layers: List[str],
                              stimuli_identifier: str,
-                             multithread: bool = False,
-                             analysis_kwargs: Dict = {}) -> Dict[str, Any]:
+                             analysis_kwargs: Optional[Dict] = None) -> Dict[str, Any]:
+        if analysis_kwargs is None:
+            analysis_kwargs = {}
         layer_results = self._run_analysis(stimuli=stimuli,
                                            layers=layers,
                                            stimuli_identifier=stimuli_identifier,
-                                           multithread=multithread,
                                            **analysis_kwargs)
         self._metadata['stimuli_identifier'] = stimuli_identifier
         return layer_results
@@ -109,23 +105,19 @@ class ModelActivationsAnalysis(ABC):
                       stimuli: Union[StimulusSet, List[str], List[Path]],
                       layers: List[str],
                       stimuli_identifier: Optional[Union[bool, str]] = None,
-                      multithread: bool = False,
                       **kwargs) -> Dict[str, Any]:
         self._logger.debug('Obtaining activations')
         layer_activations = self._activations_model(stimuli=stimuli, layers=layers,
                                                     stimuli_identifier=stimuli_identifier)
-        layer_activations = {layer: layer_activations.sel(layer=layer)
-                             for layer in np.unique(layer_activations['layer'])}
 
         self._logger.debug('Performing analyses')
         progress = tqdm(total=len(layer_activations), desc="layer analyses")
 
-        def do_layer_analysis(layer, activations):
-            result = self.analysis_func(assembly=activations, **kwargs)
+        layer_results = OrderedDict()
+        for layer in np.unique(layer_activations['layer']):
+            activations = layer_activations.sel(layer=layer)
+            layer_results[layer] = self.analysis_func(assembly=activations, **kwargs)
             progress.update(1)
-            return result
 
-        layer_results = change_dict(layer_activations, change_function=do_layer_analysis,
-                                    keep_name=True, multithread=multithread)
         progress.close()
         return layer_results
